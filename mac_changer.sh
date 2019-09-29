711f2ef24af483a06156a84d2ba91b1e08698373
CHERIEF Yassine <fy_cherief@esi.dz>
2019-09-29 12:43:23 +0100

    Adds mac_changer script based on the ip command line utility

---
+++ mac_changer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+working_macaddr="b8:ee:65:17:1f:d7";
+
+if [[ $(id -u) -ne 0 ]]; then
+	echo -e "\e[31m[ERROR] Script should be ran as root\e[0m";
+	exit 1;
+fi
+
+while getopts ":i:m:" opt; do
+  case $opt in
+    i) interface="$OPTARG"
+    ;;
+    m) mac_address="$OPTARG"
+    ;;
+    \?) echo -e "\e[31mInvalid option -$OPTARG\e[0m" >&2
+    ;;
+  esac
+done
+
+if hash ip 2>/dev/null; then
+	ip link set $interface down;
+	ip link set $interface address $mac_address;
+	ip link set $interface up;
+	echo -e "\e[32mMac address changed successfully\e[0m";
+else
+	echo -e "\e[31m[ERROR]ip command is not present on your system\e[0m";
+fi
+
+
+
+
+
+
+
+
+
